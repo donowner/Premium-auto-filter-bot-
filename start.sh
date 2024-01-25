5dcce57883ed62b14743ed2752d2d2c70d7dbb0b
donowner <150280482+donowner@users.noreply.github.com>
2024-01-25 07:43:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/ritheshrkrm/PiroAutoFilterBot.git /PiroAutoFilterBot
+  git clone https://github.com/donowner/Premium-auto-filter-bot-/blob/main/start.sh /Premium-auto-filter-bot-
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /PiroAutoFilterBot
+  git clone $UPSTREAM_REPO /Premium-auto-filter-bot-
 fi
 cd /PiroAutoFilterBot
 pip3 install -U -r requirements.txt
